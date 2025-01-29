82d45c1c87e0a0e9fe55f030ebdba56864db577b
ADILRADIDI <adulradidi@gmail.com>
2025-01-29 15:24:03 +0100

    asd

--- lib/data.ts
+++ lib/data.ts
@@ -34,29 +34,45 @@ export const links = [
 ] as const;
 
 export const experiencesData = [
+  // {
+  //   title: "Graduated bootcamp",
+  //   location: "Miami, FL",
+  //   description:
+  //     "I graduated after 6 months of studying. I immediately found a job as a front-end developer.",
+  //   icon: React.createElement(LuGraduationCap),
+  //   date: "2019",
+  // },
+  // {
+  //   title: "Front-End Developer",
+  //   location: "Orlando, FL",
+  //   description:
+  //     "I worked as a front-end developer for 2 years SXSAA in another job. I also upskilled to the full stack.",
+  //   icon: React.createElement(CgWorkAlt),
+  //   date: "2019 - 2021",
+  // },
   {
-    title: "Graduated bootcamp",
-    location: "Miami, FL",
+    title: "Full-Stack Developer Intern",
+    location: "Casablanca, Technopark - IS-TECH.",
     description:
-      "I graduated after 6 months of studying. I immediately found a job as a front-end developer.",
-    icon: React.createElement(LuGraduationCap),
-    date: "2019",
-  },
-  {
-    title: "Front-End Developer",
-    location: "Orlando, FL",
-    description:
-      "I worked as a front-end developer for 2 years SXSAA in another job. I also upskilled to the full stack.",
-    icon: React.createElement(CgWorkAlt),
-    date: "2019 - 2021",
-  },
-  {
-    title: "Full-Stack Developer",
-    location: "Houston, TX",
-    description:
-      "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
+      "Worked on various projects as a full-stack developer using Vue.js and Laravel. Developed animal management systems with RFID tracking, PDF generation for official reports, and event management solutions. Gained experience in building scalable and efficient web applications.",
     icon: React.createElement(FaReact),
-    date: "2021 - present",
+    date: "2021 - Present",
+  },  
+  {
+    title: "Full-Stack Developer for RDEV Community Platform",
+    location: "Casablanca, Technopark",
+    description:
+      "Built an innovative platform for developers, enabling post sharing, resource integration, podcast learning, tech events, and Stack Overflow Q&A integration.",
+    icon: React.createElement(FaReact),
+    date: "July 2024 - September 2024",
+  },
+  {
+    title: "Full-Stack Developer for Job Portal Platform",
+    location: "Casablanca, Technopark",
+    description:
+      "Developed a job search platform with Laravel, featuring job listings, application management, user authentication, middleware for security, and a responsive UI for optimal user experience.",
+    icon: React.createElement(CgWorkAlt),
+    date: "July 2024 - October 2024",
   },
 ] as const;
 
